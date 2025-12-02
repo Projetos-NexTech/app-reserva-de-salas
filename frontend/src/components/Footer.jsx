f3eeb54e1a82c9779dd5f448eff7776bbee641ab
pedr0almd <pdro.developer@gmail.com>
2025-12-02 14:50:21 -0300

    style(footer): adicionando estilo ao botão de 'voltar para o topo'

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import pinaLogo from "../assets/logos/logo-pina.png";
+import arrow from "../assets/icons/right-arrow.svg";
 
 function Footer() {
   return (
@@ -9,7 +10,12 @@ function Footer() {
           <img src={pinaLogo} alt="" />
           <a className="hiperlink">eventos@pinacoteca.org.br</a>
         </div>
-      <div className="backToTop"><button>teste</button><p>Voltar para o topo</p></div>
+        <div className="backToTop">
+          <button className="toRightTransition">
+            <img src={arrow} alt="seta para cima" />
+          </button>
+          <p>Voltar para o topo</p>
+        </div>
       </div>
       <div className="footer-bottom">
         <p>Design e desenvolvimento: NexTech</p>
